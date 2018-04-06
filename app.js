8e15a618ad688f79b1639588e7c2258f7d1fa7f9
JQSC <122377305@qq.com>
2018-04-06 12:13:56 +0800

    v0.1

---
+++ app.js
@@ -0,0 +1,106 @@
+//获取指定用户新浪微博信息和评论
+const request = require('request');
+const xhtml = require('./xhtml');
+const fs = require('fs')
+const cookie = require('./cookie.json');
+let header = {
+  "Accept-Language": "zh-CN,zh;q=0.9",
+  "Content-Type": "application/x-www-form-urlencoded",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+}
+//根据用户id查找指定的用户
+const userId = "2524431250"
+//如果cookie以字符串形式存储则不用cookie_str进行转化
+header.Cookie = (typeof (header) == "string") ? cookie : xhtml.cookie_str(cookie);
+//取每一页的所有数据,因为存在懒加载,所以需要将pagebar配置为0和1各取一遍
+async function main() {
+  const url = "https://weibo.com/p/aj/v6/mblog/mbloglist?domain=100505&is_search=0&visible=0&is_ori=1&is_tag=0&profile_ftype=1&pl_name=Pl_Official_MyProfileFeed__20&feed_type=0&domain_op=100505"
+  let sMaxPage = 0;
+  let pagebar = 0; //0 或者1
+  let pageDataList = [], listInfo = null, type = 1;
+  while (type) {
+    let wburl = url + `&id=100505${userId}&page=${sMaxPage}&pagebar=${pagebar}&pre_page=${sMaxPage}`;
+    listInfo = await fetchContentHtml(wburl)  //[{pubTime,mid},]
+    if (!listInfo || !listInfo.length) {
+      type = 0;//结束循环
+    } else {
+      console.info(`正在解析第${sMaxPage + 1}页第${pagebar + 1}栏`)
+      let l = pageDataList.length;
+      //首页内容列表可能不足两栏，会存在重复情况
+      if (l == 1 && pageDataList[l - 1][0].mid == listInfo[0].mid) {
+        sMaxPage++
+      } else {
+        pageDataList.push(listInfo)
+        sMaxPage = pagebar ? (++sMaxPage) : sMaxPage;
+        pagebar = pagebar ? 0 : 1;
+      }
+    }
+  }
+  console.log("页面内容获取结束!")
+  //所有页面获取结束,则开始取每一页中每一条微博的所有评论
+  return getCommentAll(pageDataList)
+}
+//整合每一页的html
+function fetchContentHtml(url) {
+  //请求配置项
+  let options = {
+    method: 'GET',
+    headers: header,
+    url: url,
+    gzip: true
+  }
+  return new Promise((resolve) => {
+    request(options, function (err, res, body) {
+      res.setEncoding('utf-8');
+      let html = JSON.parse(body).data
+      //fs.writeFile('./test.html',data, { flag: 'w', encoding: 'utf-8', mode: '0666' })
+      resolve(xhtml.getUserDetail(html))
+    })
+  });
+}
+
+
+//获取评论--评论需要每一条的mid单独发请求
+async function getCommentAll(pageDataList) {
+  //请求配置项
+  let options = {
+    method: 'GET',
+    headers: header,
+    gzip: true
+  }
+  let commentUrl = "https://weibo.com/aj/v6/comment/small?ajwvr=6&act=list&isMain=true&dissDataFromFeed=%5Bobject%20Object%5D&location=page_100505_home&comment_type=0&_t=0"
+  commentUrl += `&ouid=${userId}`
+  let commentList = [];
+  for (let arr of pageDataList) {
+    for (let o of arr) {
+      //comments   mid  &mid=4219921246273207
+      if (o.comments) {
+        options.url = commentUrl + `&mid=${o.mid}`
+        console.log(`开始取mid: ${o.mid} 的所有评论`)
+        commentList = await fetchCommentHtml(options, commentList)
+      }
+    }
+  }
+  //取到所有评论进行排序
+  xhtml.commentSort(commentList)
+  for (let v of commentList) {
+    console.log(`${v.name} 评论了 ${v.commentNum}次`)
+  }
+  return commentList;
+}
+
+function fetchCommentHtml(options, commentList) {
+  return new Promise((resolve) => {
+    request(options, function (err, res, body) {
+      res.setEncoding('utf-8');
+      let html = JSON.parse(body).data.html
+      //fs.writeFile('./test2.html',data.html, { flag: 'w', encoding: 'utf-8', mode: '0666' })
+      resolve(xhtml.getCommentDetail(html, commentList))
+    })
+  });
+}
+//comment()
+main()
+
+
+
---
+++ xhtml.js
@@ -0,0 +1,76 @@
+"use strict";
+class xhtml {
+    //将cookie对象转化为字符串格式
+    static cookie_str(oCookie) {
+        let str = "";
+        for (let v in oCookie) {
+            str += `${v}=${oCookie[v]}; `
+        }
+        return str
+    }
+    //获取所发表的每一条信息的时间和mid
+    static getUserDetail(html) {
+        const regexMid_Time = /<div class="WB_from S_txt2">[^]*?name=(\d+)[^]*?title="([^]*?)"[^]*?<\/div>/g;
+        let result, loveInfo = [];
+        while ((result = regexMid_Time.exec(html)) != null) {
+            console.log(result[1], result[2])
+            loveInfo.push({
+                pubTime: result[2],
+                mid: result[1],
+                comments: 0
+            })
+        }
+        xhtml.setComments(loveInfo, html);
+        return loveInfo
+    }
+    static setComments(arr, html) {
+        const regex_Comment = /W_ficon ficon_repeat S_ficon[^]*?<em>([^]*?)<\/em>/g
+        let i = 0, result;
+        while ((result = regex_Comment.exec(html)) != null) {
+            //console.log(result[1])
+            if (arr[i]) {
+                arr[i].comments = (result[1] == "评论") ? 0 : result[1];
+                i++
+            }
+        }
+        return arr
+    }
+    static isCommentId(id, list) {
+        if (list.length > 0) {
+            return list.some(function (o, index, array) {
+                if (o.id == id) {
+                    o.commentNum++
+                    return true
+                }
+            })
+        }
+    }
+    static getCommentDetail(html, commentList) {
+        const regex_comment = /node-type="replywrap">[^]*?usercard="id=(\d+)"[^]*?>([^]*?)<\/a>/g
+        let result;
+        //4219921246273207
+        while ((result = regex_comment.exec(html)) != null) {
+            //判斷是否包含此id
+            let isConclude = xhtml.isCommentId(result[1], commentList)
+            if (!isConclude) {
+                commentList.push(
+                    {
+                        id: result[1],
+                        name: result[2],
+                        commentNum: 1
+                    }
+                )
+            }
+            //console.log(result[1])
+        }
+        return commentList
+    }
+    static commentSort(arr) {
+        return arr.sort(function (a, b) {
+            return b.commentNum - a.commentNum
+        })
+    }
+}
+module.exports = xhtml;
+
+
